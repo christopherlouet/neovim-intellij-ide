f2fe527bd784b76fa1240644ca343df65d220f11
Christopher Louët <christopher.louet@pm.me>
2026-01-16 11:24:56 +0100

    feat(treesitter): re-enable nvim-treesitter
    Re-enable nvim-treesitter and nvim-ts-autotag plugins that were
    previously disabled. This restores syntax highlighting and
    auto-tag functionality.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,40 +1,37 @@
--- Treesitter désactivé temporairement - utilise la coloration native de Neovim
--- Pour réactiver : décommenter le code ci-dessous et exécuter :Lazy sync
-
 return {
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   build = ":TSUpdate",
-  --   event = { "BufReadPost", "BufNewFile" },
-  --   opts = {
-  --     ensure_installed = {
-  --       "lua",
-  --       "javascript",
-  --       "typescript",
-  --       "tsx",
-  --       "html",
-  --       "css",
-  --       "json",
-  --       "bash",
-  --       "markdown",
-  --       "markdown_inline",
-  --       "prisma",
-  --     },
-  --     sync_install = false,
-  --     auto_install = false,
-  --     highlight = {
-  --       enable = true,
-  --       additional_vim_regex_highlighting = false,
-  --     },
-  --     indent = {
-  --       enable = true,
-  --     },
-  --   },
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+        "html",
+        "css",
+        "json",
+        "bash",
+        "markdown",
+        "markdown_inline",
+        "prisma",
+      },
+      sync_install = false,
+      auto_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    },
+  },
 
   {
     "windwp/nvim-ts-autotag",
-    enabled = false, -- Désactivé car dépend de treesitter
+    enabled = true,
     event = "InsertEnter",
     ft = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact", "tsx", "jsx" },
     dependencies = { "nvim-treesitter/nvim-treesitter" },
