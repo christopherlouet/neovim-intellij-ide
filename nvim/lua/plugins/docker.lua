862accd6a53232cf11ca96a8e0e7213c0c1e057f
Christopher Louët <christopher.louet@pm.me>
2026-01-16 12:23:16 +0100

    refactor(audit): apply quality audit recommendations
    - perf(docker): use keys-based lazy loading instead of VeryLazy event
    - refactor(options): use vim.opt instead of vim.o for sessionoptions
    - feat(lsp): add schemastore.nvim for JSON/YAML schema validation
      - Provides 500+ schemas for common config files
      - Replaces hardcoded schema URLs in yamlls config

--- nvim/lua/plugins/docker.lua
+++ nvim/lua/plugins/docker.lua
@@ -1,19 +1,15 @@
 return {
   {
     "krisajenkins/telescope-docker.nvim",
-    event = "VeryLazy",
     dependencies = { "nvim-telescope/telescope.nvim" },
+    keys = {
+      { "<leader>Dp", "<cmd>Telescope telescope_docker docker_ps<cr>", desc = "Docker containers" },
+      { "<leader>Di", "<cmd>Telescope telescope_docker docker_images<cr>", desc = "Docker images" },
+      { "<leader>Dv", "<cmd>Telescope telescope_docker docker_volumes<cr>", desc = "Docker volumes" },
+    },
     config = function()
       require("telescope").load_extension("telescope_docker")
       require("telescope_docker").setup({})
-      vim.keymap.set("n", "<leader>Dp", "<cmd>Telescope telescope_docker docker_ps<cr>", { desc = "Docker containers" })
-      vim.keymap.set("n", "<leader>Di", "<cmd>Telescope telescope_docker docker_images<cr>", { desc = "Docker images" })
-      vim.keymap.set(
-        "n",
-        "<leader>Dv",
-        "<cmd>Telescope telescope_docker docker_volumes<cr>",
-        { desc = "Docker volumes" }
-      )
     end,
   },
 }
